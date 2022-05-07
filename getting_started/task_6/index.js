719c465a3ae803429416e579448118b0c1f295d7
OVECJOE <ovecjoe123@gmail.com>
2022-05-07 23:53:39 +0100

    Refactor task_6

--- getting_started/task_6/index.js
+++ getting_started/task_6/index.js
@@ -1,17 +1,42 @@
-/**
- * renderPunchline renders the punchline of a joke.
- * @param {*} punchline is either array or string
- * @returns rendered punchline (JSX)
- */
+const Jokes = [
+    {
+        setup: "'And by the way, you've got a lovely home!'",
+        punchline: "What's the setup ?"
+    },
+    {
+        setup: "Does anybody have a joke where the punchline would make no sense if heard alone? I'm thinking like in movies and shows where you come in on a character telling the punchline and everybody laughs hysterically.",
+        punchline: "I've always wanted to hear the setup to one of those type of jokes."
+    },
+    {
+        setup: "A football team should setup a charity that gives presents to children with Down Syndrome",
+        punchline: "...and call it Touch Downs."
+    },
+    {
+        setup: "Three people are given the death sentence...",
+        punchline: [
+            "They are given a choice, guillotine or rifle for their execution.",
+            "The first convict states he will take the guillotine. When they setup and release, the blade gets stuck and the sheriff states 'it is not your time, you may go.'",
+            "The second decides on guillotine as well, the blade gets stuck and they also let him free.",
+            'The third says "Well, since the guillotine isn\'t working, I will take death by rifle."'
+        ]
+    },
+    {
+        setup: "Request: A joke involving an Indian, an Irish, an Aussie and a Brazilian",
+        punchline: "This resumes the current setup at home, whenever I tell someone it sounds like I'm starting a joke... please help me finding a real joke to use."
+    },
+    {
+        setup: "I was kidnapped by mimes once."
+    }
+]
+
 function renderPunchline(punchline) {
     if (punchline instanceof Array) {
         return (
-            <>
+            <div>
                 {punchline.map((element) => {
-                        console.log(element);
                         return <p>{element}</p>
                     })}
-            </>
+            </div>
         )
     } else if (typeof punchline === 'string') {
         return <p>{punchline}</p>
@@ -47,37 +72,16 @@ function Joke(props) {
 }
 
 function App() {
+    const JokeElements = Jokes.map((joke) => {
+        return <Joke 
+            setup={joke.setup}
+            punchline={joke.punchline}
+        />
+    })
     return (
         <div className="jokes">
             <h1>Jokes with Setup and Punchline</h1>
-            <Joke
-                setup="'And by the way, you've got a lovely home!'"
-                punchline="What's the setup ?"
-            />
-            <Joke
-                setup="Does anybody have a joke where the punchline would make no sense if heard alone? I'm thinking like in movies and shows where you come in on a character telling the punchline and everybody laughs hysterically."
-                punchline="I've always wanted to hear the setup to one of those type of jokes."
-            />
-            <Joke
-                setup="A football team should setup a charity that gives presents to children with Down Syndrome"
-                punchline="...and call it Touch Downs."
-            />
-            <Joke
-                setup="Three people are given the death sentence..."
-                punchline={[
-                    "They are given a choice, guillotine or rifle for their execution.",
-                    "The first convict states he will take the guillotine. When they setup and release, the blade gets stuck and the sheriff states 'it is not your time, you may go.'",
-                    "The second decides on guillotine as well, the blade gets stuck and they also let him free.",
-                    'The third says "Well, since the guillotine isn\'t working, I will take death by rifle."'
-                ]}
-            />
-            <Joke
-                setup="Request: A joke involving an Indian, an Irish, an Aussie and a Brazilian"
-                punchline="This resumes the current setup at home, whenever I tell someone it sounds like I'm starting a joke... please help me finding a real joke to use."
-            />
-            <Joke
-                setup="I was kidnapped by mimes once."
-            />
+            {JokeElements}
         </div>
     )
 }
