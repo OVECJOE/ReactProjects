cbe401b3f060386a6bd69eadc6c1a5ca8989fe2b
OVECJOE <ovecjoe123@gmail.com>
2022-06-03 17:27:11 +0100

    Learnt class based components in task_10 directory

--- getting_started/task_10/index.js
+++ getting_started/task_10/index.js
@@ -1,31 +1,154 @@
-function App() {
-    const [starWarData, setStarWarData] = React.useState({});
-    const [count, setCount] = React.useState(1);
+// function App() {
+//     return (
+//         <h1>Function Component</h1>
+//     )
+// }
 
-    function handleClick(next) {
-        console.log(next)
-        setCount((prevCount) =>  {
-            return (next ? prevCount + 1 :
-            (prevCount > 1 ? prevCount - 1 : 1))})
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Header username='OVECJOE' />
+//                 <Greeting />
+//             </div>
+//         )
+//     }
+// }
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header>
+//                 <p>Welcome, {this.props.username}!</p>
+//             </header>
+//         );
+//     }
+// }
+
+// class Greeting extends React.Component {
+//     render() {
+//         const date = new Date()
+//         const hours = date.getHours()
+//         let timeOfDay = date.getHours()
+
+//         if (hours < 12) {
+//             timeOfDay = "morning"
+//         } else if (hours >= 12 && hours < 17) {
+//             timeOfDay = "afternoon"
+//         } else {
+//             timeOfDay = "night"
+//         }
+//         return (
+//             <h1>Good {timeOfDay} to you, sir or madam!</h1>
+//         )
+//     }
+// }
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             goOut: 'Yes',
+//         }
+//         this.toggleGoOut = this.toggleGoOut.bind(this)
+//     }
+
+//     toggleGoOut() {
+//         this.setState(prevState => {
+//             return {
+//                 goOut: prevState.goOut === "Yes" ? "No" : "Yes"
+//             }
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div className="state">
+//                 <h1 className="state--title">Should I go out tonight?</h1>
+//                 <div className="state--value" onClick={this.toggleGoOut}>
+//                     <h1>{this.state.goOut}</h1>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0,
+//         }
+
+//         this.add = this.add.bind(this)
+//         this.subtract = this.subtract.bind(this)
+//     }
+
+
+//     add() {
+//         this.setState(prevState => ({
+//             count: prevState.count + 1
+//         }))
+//     }
+
+//     subtract() {
+//         this.setState(prevState => ({
+//             count: prevState.count - 1
+//         }))
+//     }
+
+//     render() {
+//         return (
+//             <div className="counter">
+//                 <button className="counter--minus" onClick={this.subtract}>–</button>
+//                 <div className="counter--count">
+//                     <h1>{this.state.count}</h1>
+//                 </div>
+//                 <button className="counter--plus" onClick={this.add}>+</button>
+//             </div>
+//         )
+//     }
+// }
+
+class App extends React.Component {
+    state = {
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
     }
 
-    React.useEffect(() => {
-        fetch(`https://swapi.dev/api/people/${count}`)
-            .then(res => res.json())
-            .then(data => setStarWarData(data));
-    }, [count])
+    toggleFavorite = () => {
+        this.setState(prevState => ({
+            isFavorite: !prevState.isFavorite
+        }))
+    }
 
-    return (
-        <div className="App">
-            <div>
-                <h2>Character's ID: <span>{count}</span></h2>
-                <div className="btns">
-                    <button type="button" onClick={() => handleClick(false)}>Get Previous Character</button>
-                    <button type="button" onClick={() => handleClick(true)}>Get Next Character</button>
-                </div>
-            </div>
-            <pre>{JSON.stringify(starWarData, null, 2)}</pre>
-        </div>
-    )
+    render() {
+        let starIcon = this.state.isFavorite ? "star-filled.png" : "star-empty.png"
+
+        return (
+            <main>
+                <article className="card">
+                    <img src="./user.jpeg" className="card--image" />
+                    <div className="card--info">
+                        <img
+                            src={`./${starIcon}`}
+                            className="card--favorite"
+                            onClick={this.toggleFavorite}
+                        />
+                        <h2 className="card--name">
+                            {this.state.firstName} {this.state.lastName}
+                        </h2>
+                        <p className="card--contact">{this.state.phone}</p>
+                        <p className="card--contact">{this.state.email}</p>
+                    </div>
+    
+                </article>
+            </main>
+        )
+    }
 }
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<App type='Class' />, document.getElementById("root"));
